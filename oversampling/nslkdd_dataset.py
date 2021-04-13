08feea0bd578b7d829d106fb8f6f2b0b53c3cd09
Niv <nivgold@post.bgu.ac.il>
2021-04-13 11:36:31 +0300

    more memory-efficient full train

--- oversampling/nslkdd_dataset.py
+++ oversampling/nslkdd_dataset.py
@@ -195,10 +195,10 @@ def get_dataset(data_path, batch_size, from_disk=True):
                .batch(batch_size)
                .map(test_pack_features_vector))
 
-    train_full_ds = (tf.data.Dataset.from_tensor_slices(
-        (dict(nsl_dataset.train_features_full), nsl_dataset.train_labels_full))
-                .cache()
-                .batch(batch_size)
-                .map(train_pack_features_vector))
+    # train_full_ds = (tf.data.Dataset.from_tensor_slices(
+    #     (dict(nsl_dataset.train_features_full), nsl_dataset.train_labels_full))
+    #             .cache()
+    #             .batch(batch_size)
+    #             .map(train_pack_features_vector))
 
-    return train_ds, test_ds, train_full_ds
+    return train_ds, test_ds, nsl_dataset.train_features_full.values
