77b61d2111242ecab3b2db27fb51339eca0389a2
Niv <nivgold@post.bgu.ac.il>
2021-04-19 16:28:08 +0300

    trained new datasets

--- oversampling/run_creditcard.py
+++ oversampling/run_creditcard.py
@@ -26,34 +26,34 @@ dataset_name = 'creditcard'
 encoder_path = f'/home/nivgold/models/epochs_{EPOCHS}_{dataset_name}_encoder_weights.npy'
 decoder_path = f'/home/nivgold/models/epochs_{EPOCHS}_{dataset_name}_decoder_weights.npy'
 
-# TRAINING
-start_time = time.time()
-print("Start training...")
-solver_obj.train()
-end_training = time.time()
-print("---training finished after: ", end='')
-get_execute_time(start_time, end_training)
-# saving the trained weights
-solver_obj.save_weights(path='/home/nivgold/models', dataset_name=dataset_name)
+# # TRAINING
+# start_time = time.time()
+# print("Start training...")
+# solver_obj.train()
+# end_training = time.time()
+# print("---training finished after: ", end='')
+# get_execute_time(start_time, end_training)
+# # saving the trained weights
+# solver_obj.save_weights(path='/home/nivgold/models', dataset_name=dataset_name)
 
-# solver_obj.load_weights(encoder_path, decoder_path)
-#
-# # TEST WITHOUT TTA
-# start_time = time.time()
-# print("Start testing...")
-# accuracy, precision, recall, f_score, auc = solver_obj.test()
-# end_testing = time.time()
-# print("---testing finished after: ", end='')
-# get_execute_time(start_time, end_testing)
-#
-# # TEST WITH TTA
-# oversampling_method = "smote"
-# num_neighbors = 5
-# num_augmentations = 2
-#
-# start_time = time.time()
-# print(f"Start testing with TTA... \t {oversampling_method}, {num_neighbors} neighbors, {num_augmentations} TTA augmentations")
-# accuracy, precision, recall, f_score, auc = solver_obj.test_tta(oversampling_method, num_neighbors=num_neighbors, num_augmentations=num_augmentations, knn_data=creditcard_features_full)
-# end_tta_testing = time.time()
-# print("---TTA testing finished after: ", end='')
-# get_execute_time(start_time, end_tta_testing)
+solver_obj.load_weights(encoder_path, decoder_path)
+
+# TEST WITHOUT TTA
+start_time = time.time()
+print("Start testing...")
+accuracy, precision, recall, f_score, auc = solver_obj.test()
+end_testing = time.time()
+print("---testing finished after: ", end='')
+get_execute_time(start_time, end_testing)
+
+# TEST WITH TTA
+oversampling_method = "smote"
+num_neighbors = 5
+num_augmentations = 2
+
+start_time = time.time()
+print(f"Start testing with TTA... \t {oversampling_method}, {num_neighbors} neighbors, {num_augmentations} TTA augmentations")
+accuracy, precision, recall, f_score, auc = solver_obj.test_tta(oversampling_method, num_neighbors=num_neighbors, num_augmentations=num_augmentations, knn_data=creditcard_features_full)
+end_tta_testing = time.time()
+print("---TTA testing finished after: ", end='')
+get_execute_time(start_time, end_tta_testing)
