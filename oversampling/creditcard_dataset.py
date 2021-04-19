ba0b63b172d2d5e0f07bb25e5d4fdba6bd244576
Niv <nivgold@post.bgu.ac.il>
2021-04-19 11:24:09 +0300

    added Credit-Card-Fraud dataset

---
+++ oversampling/creditcard_dataset.py
@@ -0,0 +1,197 @@
+import pandas as pd
+import os
+import numpy as np
+import tensorflow as tf
+from datetime import datetime
+from sklearn.preprocessing import MinMaxScaler
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+
+def reduce_mem_usage(df):
+    """ iterate through all the columns of a dataframe and modify the data type
+        to reduce memory usage.
+    """
+    start_mem = df.memory_usage().sum() / 1024 ** 2
+    print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
+
+    for col in df.columns:
+        col_type = df[col].dtype
+
+        if col_type != object:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+        else:
+            df[col] = df[col].astype('category')
+
+    end_mem = df.memory_usage().sum() / 1024 ** 2
+    print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
+    print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
+
+    return df
+
+
+def train_test_split(full_df, train_ratio=0.7):
+    train_last_idx = int(len(full_df) * train_ratio)
+    train_df = full_df.iloc[:train_last_idx, :]
+    test_df = full_df.iloc[train_last_idx:, :]
+
+    return train_df, test_df
+
+
+class CreditCardDataset:
+    def __init__(self, data_dir_path, from_disk=False, disk_path='/home/nivgold/pkls/oversampling_pkls/creditcard_pkls'):
+        self.disk_path = disk_path
+
+        if from_disk:
+            print("Loading pkls...")
+
+            # loading from files
+            self.train_features = pd.read_pickle(disk_path+"/train_features_creditcard.pkl")
+            self.train_labels = pd.read_pickle(disk_path+"/train_labels_creditcard.pkl")
+
+            self.test_features = pd.read_pickle(disk_path+"/test_features_creditcard.pkl")
+            self.test_labels = pd.read_pickle(disk_path+"/test_labels_creditcard.pkl")
+
+            self.features_full = pd.read_pickle(disk_path + "/features_full_creditcard.pkl")
+            self.labels_full = pd.read_pickle(disk_path + "/labels_full_creditcard.pkl")
+
+        else:
+            file_name = 'creditcard.csv'
+            data_path = os.path.join(data_dir_path, file_name)
+            print(f'Openning {file_name}')
+            full_df = pd.read_csv(data_path)
+            full_df = self.preprocessing(full_df)
+            full_df = reduce_mem_usage(full_df)
+
+            label_col = 'Class'
+
+            self.labels_full = full_df[label_col]
+            self.features_full = full_df.drop(columns=[label_col], axis=1)
+
+            train_df, test_df = train_test_split(full_df)
+
+            # TRAIN
+
+            # filter out malicious aggregations (with label equal to 1)
+            print(f'number of train samples: {len(train_df[train_df[label_col] == 0])}')
+            train_df = train_df[train_df[label_col] == 0]
+            self.train_labels = train_df[label_col]
+            self.train_features = train_df.drop(columns=[label_col], axis=1)
+
+            # TEST
+
+            self.test_labels = test_df[label_col]
+            self.test_features = test_df.drop(columns=[label_col], axis=1)
+
+            # saving the attributes to disk
+            self.save_attributes_to_disk()
+
+    def preprocessing(self, data):
+
+        def _normalize_columns(df):
+            # min-max normalization
+            scaler = MinMaxScaler()
+            df[df.columns[:-1]] = scaler.fit_transform(df[df.columns[:-1]])
+            return df
+
+        def _agg_df(df):
+            target_col = 'Class'
+
+            # df[target_col] = np.where(df[target_col] == 'normal', 0, 1)
+
+            desired_cols = ['V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10',
+                            'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18', 'V19', 'V20',
+                            'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27', 'V28', 'Amount']
+
+            df = df[desired_cols + [target_col]]
+
+            # replacing inf values with column max
+            df = df.replace([np.inf], np.nan)
+            df = df.fillna(df.max())
+
+            return df
+
+
+        # ------ start of the preprocessing func ------
+        print('aggregating...')
+        data_df = _agg_df(data)
+        print('normalizing...')
+        data_df = _normalize_columns(data_df)
+
+        print('finished preprocessing')
+
+        return data_df
+
+    def save_attributes_to_disk(self):
+        print('saving attributes...')
+
+        # save train
+        pd.to_pickle(self.train_features, self.disk_path+"/train_features_creditcard.pkl")
+        pd.to_pickle(self.train_labels, self.disk_path+"/train_labels_creditcard.pkl")
+        # save test
+        pd.to_pickle(self.test_features, self.disk_path+"/test_features_creditcard.pkl")
+        pd.to_pickle(self.test_labels, self.disk_path+"/test_labels_creditcard.pkl")
+        # save train full
+        pd.to_pickle(self.features_full, self.disk_path + "/features_full_creditcard.pkl")
+        pd.to_pickle(self.labels_full, self.disk_path + "/labels_full_creditcard.pkl")
+
+
+def df_to_dataset(data, labels, shuffle=False, batch_size=32):
+    # df -> dataset -> cache -> shuffle -> batch -> prefetch
+    data = data.copy()
+    labels = labels.copy()
+    ds = tf.data.Dataset.from_tensor_slices((dict(data), labels)).cache()
+    if shuffle:
+        DATASET_SIZE = tf.data.experimental.cardinality(ds).numpy()
+        ds = ds.shuffle(buffer_size=DATASET_SIZE)
+    ds = ds.batch(batch_size)
+    return ds
+
+
+def train_pack_features_vector(features, labels):
+  """Pack the features into a single array."""
+  features = tf.stack(list(features.values()), axis=1)
+  return features, labels
+
+
+def test_pack_features_vector(features, labels):
+  features = tf.stack(list(features.values()), axis=1)
+  return features, labels
+
+
+def get_dataset(data_path, batch_size, from_disk=True):
+    # load the features and the labels and convert them to tf.Dataset (train and test)
+    craditcard = CreditCardDataset(data_path, from_disk=from_disk)
+
+    # train_ds = df_to_dataset(data=ids17.train_features, labels=ids17.train_labels, shuffle=shuffle, batch_size=batch_size).map(pack_features_vector)
+    train_ds = (tf.data.Dataset.from_tensor_slices(
+        (dict(craditcard.train_features), craditcard.train_labels))
+                .cache()
+                .batch(batch_size)
+                .map(train_pack_features_vector))
+
+    # test_ds = df_to_dataset(data=ids17.test_features, labels=ids17.test_labels, shuffle=shuffle, batch_size=batch_size).map(pack_features_vector)
+    test_ds = (tf.data.Dataset.from_tensor_slices(
+        (dict(craditcard.test_features), craditcard.test_labels))
+               .cache()
+               .batch(batch_size)
+               .map(test_pack_features_vector))
+
+    return train_ds, test_ds, craditcard.features_full.values
---
+++ oversampling/run_creditcard.py
@@ -0,0 +1,59 @@
+import creditcard_dataset as cc
+
+from solver import Solver
+import tensorflow as tf
+import time
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(gpus[0], True)
+
+def get_execute_time(start_time, end_time):
+    hours, rem = divmod(end_time - start_time, 3600)
+    minutes, seconds = divmod(rem, 60)
+    print("{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds))
+
+HOME_PATH = '/home/nivgold'
+EPOCHS = 100
+CREDITCARD_DIM = 29
+
+# testing ids2017 dataset
+start_time = time.time()
+creditcard_train_ds, creditcard_test_ds, creditcard_features_full = cc.get_dataset(HOME_PATH, 32, from_disk=True)
+end_train_test = time.time()
+print("---CreditCard train_ds, test_ds ready after: ", end='')
+get_execute_time(start_time, end_train_test)
+solver_obj = Solver(creditcard_train_ds, creditcard_test_ds, epochs=EPOCHS, features_dim=CREDITCARD_DIM)
+encoder_path = '/home/nivgold/models/epochs_100_creditcard_encoder_weights.npy'
+decoder_path = '/home/nivgold/models/epochs_100_creditcard_decoder_weights.npy'
+
+# TRAINING
+start_time = time.time()
+print("Start training...")
+solver_obj.train()
+end_training = time.time()
+print("---training finished after: ", end='')
+get_execute_time(start_time, end_training)
+# saving the trained weights
+solver_obj.save_weights(path='/home/nivgold/models', dataset_name='creditcard')
+
+# solver_obj.load_weights(encoder_path, decoder_path)
+#
+# # TEST WITHOUT TTA
+# start_time = time.time()
+# print("Start testing...")
+# accuracy, precision, recall, f_score, auc = solver_obj.test()
+# end_testing = time.time()
+# print("---testing finished after: ", end='')
+# get_execute_time(start_time, end_testing)
+#
+# # TEST WITH TTA
+# oversampling_method = "smote"
+# num_neighbors = 5
+# num_augmentations = 2
+#
+# start_time = time.time()
+# print(f"Start testing with TTA... \t {oversampling_method}, {num_neighbors} neighbors, {num_augmentations} TTA augmentations")
+# accuracy, precision, recall, f_score, auc = solver_obj.test_tta(oversampling_method, num_neighbors=num_neighbors, num_augmentations=num_augmentations, knn_data=creditcard_features_full)
+# end_tta_testing = time.time()
+# print("---TTA testing finished after: ", end='')
+# get_execute_time(start_time, end_tta_testing)
